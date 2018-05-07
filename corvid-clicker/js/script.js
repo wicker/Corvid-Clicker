70f166ad93bc9bb22754468641c68ced1b42ba33
Jenner Hanni <jenner@wickerbox.net>
2018-05-07 12:47:35 -0700

    Read out currentCorvid to console.log

--- corvid-clicker/js/script.js
+++ corvid-clicker/js/script.js
@@ -2,49 +2,49 @@ var model = {
   currentCorvid: null,
   corvids: [
   { clicks: 0,
-    name: Crow,
+    name: 'Crow',
     imgSrc: 'img/crow.png',
     imgAttribution: 'Dick Daniels',
     linkWikipedia: 'https://en.wikipedia.org/wiki/Northwestern_crow'
   },
   { clicks: 0,
-    name: Raven,
+    name: 'Raven',
     imgSrc: 'img/raven.png',
     imgAttribution: 'Jenner Hanni',
     linkWikipedia: 'https://en.wikipedia.org/wiki/Raven'
   },
   { clicks: 0,
-    name: Rook,
+    name: 'Rook',
     imgSrc: 'img/rook.png',
     imgAttribution: 'Brian Snelson',
     linkWikipedia: 'https://en.wikipedia.org/wiki/Rook_(bird)'
   },
   { clicks: 0,
-    name: Jackdaw,
+    name: 'Jackdaw',
     imgSrc: 'img/jackdaw.png',
     imgAttribution: 'Scott Wieman',
     linkWikipedia: 'https://en.wikipedia.org/wiki/Western_jackdaw'
   },
   { clicks: 0,
-    name: Jay,
+    name: 'Jay',
     imgSrc: 'img/jay.png',
     imgAttribution: 'Dick Daniels',
     linkWikipedia: 'https://en.wikipedia.org/wiki/Jay'
   },
   { clicks: 0,
-    name: Magpie,
+    name: 'Magpie',
     imgSrc: 'img/magpie.png',
     imgAttribution: 'Bengt Nyman',
     linkWikipedia: 'https://en.wikipedia.org/wiki/Magpie'
   },
   { clicks: 0,
-    name: Treepie,
+    name: 'Treepie',
     imgSrc: 'img/treepie.png',
     imgAttribution: 'Wagtail (Flickr)',
     linkWikipedia: 'https://en.wikipedia.org/wiki/Treepie'
   },
   { clicks: 0,
-    name: Cough,
+    name: 'Chough',
     imgSrc: 'img/chough.png',
     imgAttribution: 'Ken Billington',
     linkWikipedia: 'https://en.wikipedia.org/wiki/Chough'
@@ -52,12 +52,14 @@ var model = {
   ]
 }
 
-var clickableElem = document.getElementById('clickable');
-var displayCountElem = document.getElementById('count');
-var count = 0;
+var controller = {
 
-clickableElem.addEventListener('click', function(){
-  count++;
-  displayCountElem.innerHTML = count;
-}, false);
+  init: function() {
 
+    model.currentCorvid = model.corvids[0];
+    console.log(model.currentCorvid);
+  }
+
+}
+
+controller.init();
